22874dbc4bb03cd738484db6a67f082ad5d2be9c
Otabek Rustamov <otabek.pro@hotmail.com>
2023-06-08 23:21:54 +0500

    styles login page

--- GameHub/src/components/screens/home/Home.jsx
+++ GameHub/src/components/screens/home/Home.jsx
@@ -1,9 +1,19 @@
-
 const Home = () => {
+
+    const logout = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("userName");
+        window.location.href = "/login";
+    };
+
 
 
     return (
+      <div>
         <div>Home</div>
+        {<p>{localStorage.getItem("userName")}</p>}
+        <button onClick={logout}>logout</button>
+      </div>
     );
 };
 
--- GameHub/src/components/screens/home/Login.jsx
+++ GameHub/src/components/screens/home/Login.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
+import styles from "./home.module.css";
 
 const Login = () => {
 
@@ -8,6 +9,10 @@ const Login = () => {
   
   const handleLogin = async () => {
     try {
+      if (userName.length < 1) {
+        setError("Please enter a username with at least 1 character");
+        return;
+      }
       const response = await axios.post("http://localhost:5216/api/Login", {
         userName,
       });
@@ -24,17 +29,33 @@ const Login = () => {
   };
 
   return (
-    <div>
-      {error && <h3>{error}</h3>}
-      <h2>Login</h2>
-      <input
-        type="text"
-        value={userName}
-        onChange={(e) => setUserName(e.target.value)}
-        placeholder="Enter your username"
-      />
-      <button onClick={handleLogin}>Login</button>
-    </div>
+    <div className={styles.container}>
+      <div className={styles.form}>
+        {error && <h3 className={styles.error}>{error}</h3>}
+        <h2>Welcome to GameHUB</h2>
+        <div className="">
+          <input
+            type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            placeholder="Enter your username"
+            className="form-control my-3"
+          />
+        </div>
+        <button className="btn btn-primary" onClick={handleLogin}>
+          Login
+        </button>
+      </div>
+        <iframe
+          width="100vh"
+          height="100vh"
+          src="https://www.youtube.com/embed/yNcH2PuiHw8?controls=0&autoplay=1&mute=1"
+          title="YouTube video player"
+          frameBorder="0"
+          //allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
   );
 };
 
